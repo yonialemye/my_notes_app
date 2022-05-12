30cc366ed44a5bac7f7270b7a9bdadbec0b5df5a
Yonas Alemayehu <yonasalem121@gmail.com>
2022-05-12 20:20:37 +0300

    verify the email address

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_notes_app/firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -30,65 +28,47 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+    return Column(
+      children: [
+        TextField(
+          controller: email,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(
+            hintText: 'Enter your email',
+          ),
         ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: email,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your email',
-                    ),
-                  ),
-                  TextField(
-                    controller: password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your password',
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = this.email.text;
-                      final password = this.password.text;
+        TextField(
+          controller: password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration: const InputDecoration(
+            hintText: 'Enter your password',
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final email = this.email.text;
+            final password = this.password.text;
 
-                      try {
-                        final userCredential =
-                            await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          print('weak password is detected');
-                        } else if (e.code == 'email-already-in-use') {
-                          print('Email already used!');
-                        } else if (e.code == 'invalid-email') {
-                          print('Invalid email detected');
-                        }
-                      }
-                    },
-                    child: const Text('Register'),
-                  ),
-                ],
+            try {
+              final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email,
+                password: password,
               );
-            default:
-              return const Text('Loading');
-          }
-        },
-      ),
+            } on FirebaseAuthException catch (e) {
+              if (e.code == 'weak-password') {
+                print('weak password is detected');
+              } else if (e.code == 'email-already-in-use') {
+                print('Email already used!');
+              } else if (e.code == 'invalid-email') {
+                print('Invalid email detected');
+              }
+            }
+          },
+          child: const Text('Register'),
+        ),
+      ],
     );
   }
 }
