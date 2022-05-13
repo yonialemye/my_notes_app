4ce999e7bc7e6a3841fe150c7a76468127612aa0
Yonas Alemayehu <yonasalem121@gmail.com>
2022-05-13 20:08:04 +0300

    create a homepage to manage login, register and verify pages and go to the main ui

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatelessWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify Email'),
+      ),
+      body: Column(
+        children: [
+          const Text('Please verify your email address'),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+              user?.reload();
+            },
+            child: const Text('Send email verification'),
+          ),
+        ],
+      ),
+    );
+  }
+}
